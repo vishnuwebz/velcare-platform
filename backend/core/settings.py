40e02c4daff6eb1e25e2b30f2fec167b7938edf3
Vishnu G <vishnugkannath@gmail.com>
2026-03-21 01:46:43 +0530

    feat(api): add DRF serializers and base configuration

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -125,3 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        # later we can add JWT here
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
+}
---
+++ backend/users/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from .models import User, WorkerProfile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "role",
+            "phone",
+            "region",
+            "membership_tier",
+        ]
+        read_only_fields = ("id", "role", "membership_tier")
+
+
+class WorkerProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = WorkerProfile
+        fields = [
+            "id",
+            "user",
+            "zone",
+            "specialization",
+            "status",
+            "rating",
+            "total_jobs",
+        ]
+        read_only_fields = ("id", "rating", "total_jobs")
